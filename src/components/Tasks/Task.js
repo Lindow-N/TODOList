f142ce546d9eb36696acf233b593f64378cc93e2
Vincent Ferniot <vincent-ferniot@tpz.fr>
2021-01-28 10:13:01 +0100

    adaptation des styles CSS en SASS

--- src/components/Tasks/Task.js
+++ src/components/Tasks/Task.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Task = () => (
-  <li className="task task--done">
+  <li className="task">
     <input type="checkbox" className="task__checkbox" id="Faire une todolist en JS" />
     <label htmlFor="Faire une todolist en JS">Faire une todolist en JS</label>
   </li>
