7eefde687b5fb91a4618cb10ff7c5610ef0e36c2
Vincent Ferniot <vincent-ferniot@tpz.fr>
2021-01-28 11:18:47 +0100

    version condensée de Tasks

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -4,37 +4,47 @@ import PropTypes from 'prop-types';
 import Task from './Task';
 import './style.scss';
 
-const Tasks = ({ tasks }) => {
-  const tasksList = tasks.map((task) => {
-    console.log('object');
-
-    // React.createElement(Task, {...task});
-    return (
+const Tasks = ({ tasks }) => (
+  <ul className="tasks">
+    {tasks.map((task) => (
       <Task
         key={task.id}
-        // id={task.id}
-        // label={task.label}
-        // done={task.done}
-        // ici on vient utiliser le spread operator pour déverser
-        // les propriétés de l'objet task dans les props du composant Task
         {...task}
       />
-    );
-  });
+    ))}
+  </ul>
+);
 
-  return (
-    <ul className="tasks">
-      {tasksList}
-    </ul>
-  );
-};
+// version longue
+// const Tasks = ({ tasks }) => {
+//   const tasksList = tasks.map((task) => {
+//     console.log('object');
+
+//     // React.createElement(Task, {...task});
+//     return (
+//       <Task
+//         key={task.id}
+//         // id={task.id}
+//         // label={task.label}
+//         // done={task.done}
+//         // ici on vient utiliser le spread operator pour déverser
+//         // les propriétés de l'objet task dans les props du composant Task
+//         {...task}
+//       />
+//     );
+//   });
+
+//   return (
+//     <ul className="tasks">
+//       {tasksList}
+//     </ul>
+//   );
+// };
 
 Tasks.propTypes = {
   tasks: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
-      // label: PropTypes.string.isRequired,
-      // done: PropTypes.bool.isRequired,
     }),
   ).isRequired,
 };
