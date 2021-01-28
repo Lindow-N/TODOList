4b9a4d105b7c81128c1284c640499ece9667d58d
Vincent Ferniot <vincent-ferniot@tpz.fr>
2021-01-28 11:36:46 +0100

    mise en place du handler pour la soumission du form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 import './style.scss';
 
-const Form = () => (
-  <form className="form">
-    <input
-      type="text"
-      placeholder="Ajouter une tâche"
-      className="form__input"
-    />
-  </form>
-);
+const Form = () => {
+  const handleOnSubmit = (event) => {
+    // comme en vanilla il faut arrêter le comportement par défaut
+    // du formulaire
+    event.preventDefault();
+    console.log('je soumet le formulaire');
+  };
+
+  return (
+    <form onSubmit={handleOnSubmit} className="form">
+      <input
+        type="text"
+        placeholder="Ajouter une tâche"
+        className="form__input"
+      />
+    </form>
+  );
+};
 
 export default Form;
