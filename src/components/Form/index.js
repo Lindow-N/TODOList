8e983d1f1af6cf86b4fdb5f23f2f0e12928b5bd4
Vincent Ferniot <vincent-ferniot@tpz.fr>
2021-01-28 14:51:33 +0100

    focus sur l'input

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,6 +4,21 @@ import PropTypes from 'prop-types';
 import './style.scss';
 
 class Form extends React.Component {
+  // on crée une propriété de classe qui prendra la référence du composant
+  // une fois inséré dans le DOM
+  // https://fr.reactjs.org/docs/refs-and-the-dom.html
+  taskInput = React.createRef();
+
+  componentDidMount() {
+    // ici je veux mettre mon champs texte en focus
+    // avec la méthode impérative
+    // const input = document.querySelector('input[type="text"');
+    // input.focus();
+
+    // avec la méthode React
+    this.taskInput.current.focus();
+  }
+
   handleOnSubmit = (event) => {
     // les props dans une classe deviennent partie du contexte
     // il faut bien mettre this.props
@@ -32,6 +47,9 @@ class Form extends React.Component {
     return (
       <form onSubmit={this.handleOnSubmit} className="form">
         <input
+          // je viens donner la référence à mon composant
+          // pour y avoir accès une fois dans le DOM
+          ref={this.taskInput}
           type="text"
           placeholder="Ajouter une tâche"
           className="form__input"
